710a3e9d0de467ea9be2397f169640dc19017152
Gabriel Caldas <100331767+GabCaldas@users.noreply.github.com>
2022-04-25 15:21:21 -0300

    Update caldas.py

--- caldas.py
+++ caldas.py
@@ -1,43 +1,54 @@
+#LISTAS UTILIZADAS!
 manifest = []
 suges = []
 reclama = []
 elogio = []
 
 
+#FUNÇÃO PARA LISTAR MANIFESTAÇÕES
 def opcao1():
-        if manifest == []:
-            print("Não existem manifestações feitas!")
+    if manifest == []:
+        print("Não existem manifestações feitas!")
+    else:
         print("Essas são suas manifestações")
         print("")
         for mani in range(len(manifest)):
             print(
-                f" Protocolo: {manifest[mani][0]} \n Requisitante: {manifest[mani][1]} \n Tipo: {manifest[mani][2]} \n Descrição: {manifest[mani][3]}")
+            f" Protocolo: {manifest[mani][0]} \n Requisitante: {manifest[mani][1]} \n Tipo: {manifest[mani][2]} \n Descrição: {manifest[mani][3]}")
             print("")
-
-
+#FUNÇÃO PARA LISTAR APENAS SUGESTÕES
 def opcao2():
-    print("Essas são suas sugestões")
-    for sugi in range(len(suges)):
-        print(f" Protocolo: {suges[sugi][0]} \n Requisitante: {suges[sugi][1]} \n Descrição: {suges[sugi][2]}")
+    if suges == []:
+        print("Não existem sugestões feitas!")
+    else:
+        print("Essas são suas sugestões")
+        for sugi in range(len(suges)):
+            print(f" Protocolo: {suges[sugi][0]} \n Requisitante: {suges[sugi][1]} \n Descrição: {suges[sugi][2]}")
         print("")
 
-
+#FUNÇÃO PARA LISTAR APENAS RECLAMAÇÕES
 def opcao3():
-    print("Essas são suas reclamações")
-    for rec in range(len(reclama)):
-        print(f" Protocolo: {reclama[rec][0]} \n Requisitante: {reclama[rec][1]} \n Descrição: {reclama[rec][2]}")
+    if reclama == []:
+        print("Não existem reclamações feitas!")
+    else:
+        print("Essas são suas reclamações")
+        for rec in range(len(reclama)):
+            print(f" Protocolo: {reclama[rec][0]} \n Requisitante: {reclama[rec][1]} \n Descrição: {reclama[rec][2]}")
         print("")
 
-
+#FUNÇÃO PARA LISTAR APENAS ELOGIOS
 def opcao4():
-    print("Esses são seus elogios")
-    for elo in range(len(elogio)):
-        print(f" Protocolo: {elogio[elo][0]} \n Requisitante: {elogio[elo][1]} \n Descrição: {elogio[elo][2]}")
+    if elogio == []:
+        print("Não existem elogios feitos!")
+    else:
+        print("Esses são seus elogios")
+        for elo in range(len(elogio)):
+            print(f" Protocolo: {elogio[elo][0]} \n Requisitante: {elogio[elo][1]} \n Descrição: {elogio[elo][2]}")
         print("")
 
-
+#FUNÇÃO DE BUSCA POR PROTOCOLO
 def opcao6():
-    protnumero = int(input("Digite o número do seu protocolo!: "))
+    protnumero = int(input("Digite o número do seu protocolo!  "))
     # CONVERSOR DE STR PARA INT, DE MODO QUE SEJA POSSÍVEL UTILIZAR OS SINAIS MATEMÁTICOS PARA DETERMINAR O PROTOCOLO
     conversao = int(manifest[protnumero - 1][0])
     if protnumero == conversao:
@@ -46,36 +57,37 @@ def opcao6():
 
 
 
-
+#TELA INICIAL DE ESCOLHAS
 while True:
-    print("=" * 40)
-    print("""          SISTEMA DE OUVIDORIA
-        1) Listar manifestações
-        2) Listar sugestões
-        3) Listar reclamações
-        4) Listar elogios
-        5) Enviar uma manifestação 
-        6) Pesquisar protocolo por número
-        7) Sair do programa""")
-    print("=" * 40)
-    opcoes = (str(input("Digite o número correspondente à ação: ")))
+    print("=" * 60)
+    print(f"""                   SISTEMA DE OUVIDORIA ABC""")
+    print("=" * 60)
+    print(f"""                    1) Listar manifestações
+                    2) Listar sugestões
+                    3) Listar reclamações
+                    4) Listar elogios
+                    5) Enviar uma manifestação 
+                    6) Pesquisar protocolo por número
+                    7) Sair do programa""")
+    print("=" * 60)
+    opcoes = (str(input("Digite o número correspondente a ação: ")))
     if opcoes in "1234567":
         opcoes=int(opcoes)
     else:
         print("Entrada inválida, tente digitar de 1 a 7!")
         continue
-    print("=" * 40)
+    print("=" * 60)
     if opcoes < 1 or opcoes > 7:
         print("Número inválido!")
     if opcoes == 7:
-        print("=" * 40)
+        print("=" * 60)
         print("Encerrando!")
-        print("=" * 40)
+        print("=" * 60)
         break
 
     #LISTAR MANIFESTAÇÕES
     if opcoes==1:
-        opçao1()
+        opcao1()
 
     #LISTAR APENAS SUGESTÕES
     if opcoes == 2:
@@ -88,20 +100,22 @@ while True:
     #LISTAR APENAS ELOGIOS
     if opcoes == 4:
         opcao4()
-    #PROCURAR POR PROTOCOLO
+    #BUSCA PROTOCOLO
     if opcoes==6:
         opcao6()
 
     #INTRODUÇÃO PARA LISTAR MANIFESTAÇÕES
     if opcoes == 5:
-        nome = str(input("Qual é o seu nome?: ")).capitalize()
+        nome = str(input("Qual é o seu nome?  ")).capitalize()
+        print("=" * 60)
         bool=True
         while bool:
-            tipo =input(f"""Olá {nome}! qual tipo de manifestação você quer enviar?
+            tipo =input(f"""Olá, {nome}! Qual tipo de manifestação você quer enviar?
     1) RECLAMAÇÃO
     2) SUGESTÃO
     3) ELOGIO
     Digite o número correspondente: """)
+    #VALIDAÇÃO CONTRA STRINGS E NÚMEROS INCORRESPONDENTES NA ESCOLHA DO TIPO
             if tipo in '123':
                 tipo = int(tipo)
             else:
@@ -112,11 +126,13 @@ while True:
             else:
                 print("ENTRADA INVÁLIDA! Digite apenas 1, 2 ou 3!")
 
-            #VARIÁVEL PARA TRANSFORMAR OS NÚMEROS NAS MANIFESTAÇÕES CORRESPONDENTES
+    #VARIÁVEL PARA TRANSFORMAR OS NÚMEROS NAS MANIFESTAÇÕES CORRESPONDENTES
         novotipo=""
         if tipo == 1:
             novotipo = "Reclamação"
+            print("=" * 60)
             descri = str(input("Digite uma descrição sobre a sua reclamação: ")).capitalize()
+            print("=" * 60)
             protocolo = str(len(manifest) + 1)
             manifesta = protocolo + "#" + nome + "#" + novotipo + "#" + descri
             maniquebrada = manifesta.split("#")
@@ -124,10 +140,12 @@ while True:
             recll = protocolo + "#" + nome + "#" + descri
             recllquebrado = recll.split("#")
             reclama.append(recllquebrado)
-            print(f"Sua reclamação foi listada, {nome}!O número do seu protocolo é {protocolo}")
+            print(f"Sua reclamação foi listada, {nome}! O número do seu protocolo é {protocolo}")
         if tipo == 2:
             novotipo = "Sugestão"
+            print("=" * 60)
             descri = str(input("Digite sua sugestão!: ")).capitalize()
+            print("=" * 60)
             protocolo = str(len(manifest) + 1)
             manifesta = protocolo + "#" + nome + "#" + novotipo + "#" + descri
             maniquebrada = manifesta.split("#")
@@ -138,7 +156,9 @@ while True:
             print(f"Sua sugestão foi listada, {nome}! O número do seu protocolo é {protocolo}")
         if tipo == 3:
             novotipo = "Elogio"
+            print("=" * 60)
             descri = str(input("Digite seu elogio!: ")).capitalize()
+            print("=" * 60)
             protocolo = str(len(manifest) + 1)
             manifesta = protocolo + "#" + nome + "#" + novotipo + "#" + descri
             maniquebrada = manifesta.split("#")
@@ -146,4 +166,4 @@ while True:
             ello = protocolo + "#" + nome + "#" + descri
             elloquebrado = ello.split("#")
             elogio.append(elloquebrado)
-            print(f"Seu elogio foi enviado,obrigado {nome}! O número do seu protocolo é {protocolo}")
+            print(f"Seu elogio foi enviado. Obrigado {nome}! O número do seu protocolo é {protocolo}")
